364a666e764aef708f8475c3828c469a9b6efb50
Julius Kiano <jgkiano@gmail.com>
2023-08-03 11:44:28 +0300

    added integration example

--- App.tsx
+++ App.tsx
@@ -1,49 +1,98 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, ActivityIndicator} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
+import {
+  OkHiUser,
+  OkCollectSuccessResponse,
+  OkHiLocationManager,
+  request,
+  isLocationServicesEnabled,
+  requestNotificationPermission,
+  onNewToken,
+} from 'react-native-okhi';
+
+let USER: OkHiUser = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  phone: '+254700000000',
+  email: 'jane@myemail.com',
+};
 
 const App = () => {
-  const [token, setToken] = useState<null | string>(null);
+  const [launch, setLaunch] = useState(false);
 
   useEffect(() => {
-    return messaging().onTokenRefresh(setToken);
+    return messaging().onTokenRefresh(onNewToken);
   }, []);
 
-  const isNotificationPermissionGranted = async () => {
-    const result = await messaging().hasPermission();
+  const fetchPushNotificationToken = async () => {
+    return messaging().getToken();
+  };
+
+  const handleVerifyAddress = async () => {
+    const locationServicesAvailable = await isLocationServicesEnabled();
+    if (locationServicesAvailable) {
+      request('always', null, async (status, _) => {
+        if (status === 'authorizedAlways') {
+          const notificationPermissionStatus =
+            await requestNotificationPermission();
+          if (notificationPermissionStatus) {
+            const fcmToken = await fetchPushNotificationToken();
+            USER.fcmPushNotificationToken = fcmToken;
+            setLaunch(true);
+          }
+        }
+      });
+    }
+  };
+
+  const handleOnSuccess = async (response: OkCollectSuccessResponse) => {
+    response
+      .startVerification()
+      .then(locationId => {
+        console.log('started verification for: ' + locationId);
+      })
+      .catch(error => {
+        console.log(error.code);
+        console.log(error.message);
+      })
+      .finally(() => {
+        setLaunch(false);
+      });
+  };
+
+  const renderLoader = () => {
     return (
-      result === messaging.AuthorizationStatus.AUTHORIZED ||
-      result === messaging.AuthorizationStatus.PROVISIONAL
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <ActivityIndicator color="teal" />
+      </View>
     );
-  };
-
-  const handleNotificationPermissionRequest = async () => {
-    const result = await isNotificationPermissionGranted();
-    if (!result) {
-      await messaging().requestPermission();
-    }
-  };
-
-  const handleFetchNotificationTokenRequest = async () => {
-    const result = await isNotificationPermissionGranted();
-    if (result) {
-      messaging().getToken().then(setToken);
-    }
   };
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        title="Request Notification Permission"
-        onPress={handleNotificationPermissionRequest}
+      <Button title="Verify an address" onPress={handleVerifyAddress} />
+      <OkHiLocationManager
+        launch={launch}
+        user={{...USER}}
+        onCloseRequest={() => setLaunch(false)}
+        onError={console.log}
+        onSuccess={handleOnSuccess}
+        config={{streetView: true}}
+        loader={renderLoader()}
+        theme={{
+          appBar: {
+            backgroundColor: '#333',
+            logo: 'https://cdn.okhi.co/icon.png',
+          },
+          colors: {
+            primary: '#333',
+          },
+        }}
       />
-      <Button
-        title="Fetch Notification Token"
-        onPress={handleFetchNotificationTokenRequest}
-      />
-      {token ? <Text>{token}</Text> : null}
     </View>
   );
 };
+
 export default App;
