14f40bcc21c5995222c611036914b20b57e788d4
Julius Kiano <jgkiano@gmail.com>
2023-08-03 11:48:04 +0300

    added okhi ios config

--- ios/OkHiPushNotificationExample/AppDelegate.h
+++ ios/OkHiPushNotificationExample/AppDelegate.h
@@ -1,9 +1,11 @@
 #import <Firebase.h>
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+@import OkHi;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
+@property (strong, nonatomic) OkVerify *okverify;
 
 @end
